f0f50c3fdae94ba34f70e11c24a5fc597ea27e59
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-03-26 08:37:40 +0200

    Debug

--- header.h
+++ header.h
@@ -1,9 +1,12 @@
 //
 // Created by zdene on 25.03.2018.
 //
-#include <string>
 #ifndef UNTITLED_HEADER_H
 #define UNTITLED_HEADER_H
+#include <iostream>
+#include <string>
+using namespace std;
+
 
 class world_list {
  public:
@@ -17,11 +20,10 @@ class world_list {
     string Birthday_Son;
     char choose;
 
-    void world_list::write_in_file(Name,Surname,Date,Pet,Daughter,Daughter_Birthday,Son,Birthday_Son){};
-    void world_list::world_list(){};
-    void world_list::choose_form(Name,Surname,Date,Pet,Daughter,Daughter_Birthday,Son,Birthday_Son){};
-    void world_list::start_choose(choose){};
-    void world_list::exiting_program(){};
+    void write_in_file(string Name,string Surname,string Date,string Pet,string Daughter,string Daughter_Birthday,string Son,string Birthday_Son);
+    void choose_form(string Name,string Surname,string Date,string Pet,string Daughter,string Daughter_Birthday,string Son,string Birthday_Son);
+    void start_choose(char choose);
+    void exiting_program();
 
 
 };
