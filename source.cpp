47eb754c03114624ecc1e1d5a28a51fa4857b232
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-04-10 10:35:17 +0200

    Update combinations

--- source.cpp
+++ source.cpp
@@ -33,356 +33,84 @@ void world_list::choose_form(string Name,string Surname,string Date,string Pet,s
     cin >> Son;
     cout << "Birthday Son"<<endl;
     cin >> Birthday_Son;
-
     cout << "I'm creating file with WorldList, wait idiot!";
     world_list::write_in_file(Name,Surname,Date,Pet,Daughter,Daughter_Birthday,Son,Birthday_Son);
 };
 
+void world_list::convert_to_Ascii(char help){
+/*A-Z 65-90
+a-z   97-122*/
+int world_list::My=0;
+int oneChar = (int) help;
+char world_list::Array[world_list::My] = (char) oneChar - 32;
+world_list::My++;
+
+}
+
+void world_list::combination_two_stringConvert(string help){
+ofstream file("World_List_Array_My.txt");
+int x = 0;
+int y = 0;
+while(x==0){
+    if(help[y]){
+    world_list::convert_to_Ascii(help[y]);
+    file<<world_list::Array[world_list::My]<<endl;
+        y++;
+    }else{
+    x++;
+    }
+ }
+}
+
+void world_list::combination_one(string help,string help1){
+    ofstream file("World_List_Array_My.txt");
+    file<<help<<endl;
+    file<<help1<<endl;
+    file<<help<<help1<<endl;
+    file<<help1<<help<<endl;
+    file<<help<<" "<<help1<<endl;
+    file<<help1<<" "<<help<<endl;
+    file<<help<<"_"<<help1<<endl;
+    file<<help1<<"_"<<help<<endl;
+    file<<help<<"."<<help1<<endl;
+    file<<help1<<"."<<help<<endl;
+}
 
 void world_list::write_in_file(string Name,string Surname,string Date,string Pet,string Daughter,string Daughter_Birthday,string Son,string Birthday_Son){
-    ofstream file("World_List_Array_My.txt");
-
-    file <<Name<<endl;
-    file <<Surname<<endl;
-    file <<Date<<endl;
-    file <<Pet<<endl;
-    file <<Daughter<<endl;
-    file <<Daughter_Birthday<<endl;
-    file <<Son<<endl;
-    file <<Birthday_Son<<endl;
-
-    /*Name generating*/
-    file <<Name<<Surname<<endl;
-    file <<Name<<Date<<endl;
-    file <<Name<<Pet<<endl;
-    file <<Name<<Daughter<<endl;
-    file <<Name<<Daughter_Birthday<<endl;
-    file <<Name<<Son<<endl;
-    file <<Name<<Birthday_Son<<endl;
-    /*_*/
-    file <<Name<<"_"<<Surname<<endl;
-    file <<Name<<"_"<<Date<<endl;
-    file <<Name<<"_"<<Pet<<endl;
-    file <<Name<<"_"<<Daughter<<endl;
-    file <<Name<<"_"<<Daughter_Birthday<<endl;
-    file <<Name<<"_"<<Son<<endl;
-    file <<Name<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Name<<"-"<<Surname<<endl;
-    file <<Name<<"-"<<Date<<endl;
-    file <<Name<<"-"<<Pet<<endl;
-    file <<Name<<"-"<<Daughter<<endl;
-    file <<Name<<"-"<<Daughter_Birthday<<endl;
-    file <<Name<<"-"<<Son<<endl;
-    file <<Name<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Name<<"."<<Surname<<endl;
-    file <<Name<<"."<<Date<<endl;
-    file <<Name<<"."<<Pet<<endl;
-    file <<Name<<"."<<Daughter<<endl;
-    file <<Name<<"."<<Daughter_Birthday<<endl;
-    file <<Name<<"."<<Son<<endl;
-    file <<Name<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Name<<" "<<Surname<<endl;
-    file <<Name<<" "<<Date<<endl;
-    file <<Name<<" "<<Pet<<endl;
-    file <<Name<<" "<<Daughter<<endl;
-    file <<Name<<" "<<Daughter_Birthday<<endl;
-    file <<Name<<" "<<Son<<endl;
-    file <<Name<<" "<<Birthday_Son<<endl;
-
-    /*Surname generating*/
-    file <<Surname<<Name<<endl;
-    file <<Surname<<Date<<endl;
-    file <<Surname<<Pet<<endl;
-    file <<Surname<<Daughter<<endl;
-    file <<Surname<<Daughter_Birthday<<endl;
-    file <<Surname<<Son<<endl;
-    file <<Surname<<Birthday_Son<<endl;
-    /*_*/
-    file <<Surname<<"_"<<Name<<endl;
-    file <<Surname<<"_"<<Date<<endl;
-    file <<Surname<<"_"<<Pet<<endl;
-    file <<Surname<<"_"<<Daughter<<endl;
-    file <<Surname<<"_"<<Daughter_Birthday<<endl;
-    file <<Surname<<"_"<<Son<<endl;
-    file <<Surname<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Surname<<"-"<<Name<<endl;
-    file <<Surname<<"-"<<Date<<endl;
-    file <<Surname<<"-"<<Pet<<endl;
-    file <<Surname<<"-"<<Daughter<<endl;
-    file <<Surname<<"-"<<Daughter_Birthday<<endl;
-    file <<Surname<<"-"<<Son<<endl;
-    file <<Surname<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Surname<<"."<<Name<<endl;
-    file <<Surname<<"."<<Date<<endl;
-    file <<Surname<<"."<<Pet<<endl;
-    file <<Surname<<"."<<Daughter<<endl;
-    file <<Surname<<"."<<Daughter_Birthday<<endl;
-    file <<Surname<<"."<<Son<<endl;
-    file <<Surname<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Surname<<" "<<Name<<endl;
-    file <<Surname<<" "<<Date<<endl;
-    file <<Surname<<" "<<Pet<<endl;
-    file <<Surname<<" "<<Daughter<<endl;
-    file <<Surname<<" "<<Daughter_Birthday<<endl;
-    file <<Surname<<" "<<Son<<endl;
-    file <<Surname<<" "<<Birthday_Son<<endl;
-
-    /*Date generating*/
-    file <<Date<<Name<<endl;
-    file <<Date<<Surname<<endl;
-    file <<Date<<Pet<<endl;
-    file <<Date<<Daughter<<endl;
-    file <<Date<<Daughter_Birthday<<endl;
-    file <<Date<<Son<<endl;
-    file <<Date<<Birthday_Son<<endl;
-    /*_*/
-    file <<Date<<"_"<<Name<<endl;
-    file <<Date<<"_"<<Surname<<endl;
-    file <<Date<<"_"<<Pet<<endl;
-    file <<Date<<"_"<<Daughter<<endl;
-    file <<Date<<"_"<<Daughter_Birthday<<endl;
-    file <<Date<<"_"<<Son<<endl;
-    file <<Date<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Date<<"-"<<Name<<endl;
-    file <<Date<<"-"<<Surname<<endl;
-    file <<Date<<"-"<<Pet<<endl;
-    file <<Date<<"-"<<Daughter<<endl;
-    file <<Date<<"-"<<Daughter_Birthday<<endl;
-    file <<Date<<"-"<<Son<<endl;
-    file <<Date<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Date<<"."<<Name<<endl;
-    file <<Date<<"."<<Surname<<endl;
-    file <<Date<<"."<<Pet<<endl;
-    file <<Date<<"."<<Daughter<<endl;
-    file <<Date<<"."<<Daughter_Birthday<<endl;
-    file <<Date<<"."<<Son<<endl;
-    file <<Date<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Date<<" "<<Name<<endl;
-    file <<Date<<" "<<Surname<<endl;
-    file <<Date<<" "<<Pet<<endl;
-    file <<Date<<" "<<Daughter<<endl;
-    file <<Date<<" "<<Daughter_Birthday<<endl;
-    file <<Date<<" "<<Son<<endl;
-    file <<Date<<" "<<Birthday_Son<<endl;
-
-    /*Pet generating*/
-    file <<Pet<<Name<<endl;
-    file <<Pet<<Surname<<endl;
-    file <<Pet<<Date<<endl;
-    file <<Pet<<Daughter<<endl;
-    file <<Pet<<Daughter_Birthday<<endl;
-    file <<Pet<<Son<<endl;
-    file <<Pet<<Birthday_Son<<endl;
-    /*_*/
-    file <<Pet<<"_"<<Name<<endl;
-    file <<Pet<<"_"<<Surname<<endl;
-    file <<Pet<<"_"<<Date<<endl;
-    file <<Pet<<"_"<<Daughter<<endl;
-    file <<Pet<<"_"<<Daughter_Birthday<<endl;
-    file <<Pet<<"_"<<Son<<endl;
-    file <<Pet<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Pet<<"-"<<Name<<endl;
-    file <<Pet<<"-"<<Surname<<endl;
-    file <<Pet<<"-"<<Date<<endl;
-    file <<Pet<<"-"<<Daughter<<endl;
-    file <<Pet<<"-"<<Daughter_Birthday<<endl;
-    file <<Pet<<"-"<<Son<<endl;
-    file <<Pet<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Pet<<"."<<Name<<endl;
-    file <<Pet<<"."<<Surname<<endl;
-    file <<Pet<<"."<<Date<<endl;
-    file <<Pet<<"."<<Daughter<<endl;
-    file <<Pet<<"."<<Daughter_Birthday<<endl;
-    file <<Pet<<"."<<Son<<endl;
-    file <<Pet<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Pet<<" "<<Name<<endl;
-    file <<Pet<<" "<<Surname<<endl;
-    file <<Pet<<" "<<Date<<endl;
-    file <<Pet<<" "<<Daughter<<endl;
-    file <<Pet<<" "<<Daughter_Birthday<<endl;
-    file <<Pet<<" "<<Son<<endl;
-    file <<Pet<<" "<<Birthday_Son<<endl;
-
-    /*Daughter generating*/
-    file <<Daughter<<Name<<endl;
-    file <<Daughter<<Surname<<endl;
-    file <<Daughter<<Date<<endl;
-    file <<Daughter<<Pet<<endl;
-    file <<Daughter<<Daughter_Birthday<<endl;
-    file <<Daughter<<Son<<endl;
-    file <<Daughter<<Birthday_Son<<endl;
-    /*_*/
-    file <<Daughter<<"_"<<Name<<endl;
-    file <<Daughter<<"_"<<Surname<<endl;
-    file <<Daughter<<"_"<<Date<<endl;
-    file <<Daughter<<"_"<<Pet<<endl;
-    file <<Daughter<<"_"<<Daughter_Birthday<<endl;
-    file <<Daughter<<"_"<<Son<<endl;
-    file <<Daughter<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Daughter<<"-"<<Name<<endl;
-    file <<Daughter<<"-"<<Surname<<endl;
-    file <<Daughter<<"-"<<Date<<endl;
-    file <<Daughter<<"-"<<Pet<<endl;
-    file <<Daughter<<"-"<<Daughter_Birthday<<endl;
-    file <<Daughter<<"-"<<Son<<endl;
-    file <<Daughter<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Daughter<<"."<<Name<<endl;
-    file <<Daughter<<"."<<Surname<<endl;
-    file <<Daughter<<"."<<Date<<endl;
-    file <<Daughter<<"."<<Pet<<endl;
-    file <<Daughter<<"."<<Daughter_Birthday<<endl;
-    file <<Daughter<<"."<<Son<<endl;
-    file <<Daughter<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Daughter<<" "<<Name<<endl;
-    file <<Daughter<<" "<<Surname<<endl;
-    file <<Daughter<<" "<<Date<<endl;
-    file <<Daughter<<" "<<Pet<<endl;
-    file <<Daughter<<" "<<Daughter_Birthday<<endl;
-    file <<Daughter<<" "<<Son<<endl;
-    file <<Daughter<<" "<<Birthday_Son<<endl;
-
-    /*Daughter_Birthday generating*/
-    file <<Daughter_Birthday<<Name<<endl;
-    file <<Daughter_Birthday<<Surname<<endl;
-    file <<Daughter_Birthday<<Date<<endl;
-    file <<Daughter_Birthday<<Pet<<endl;
-    file <<Daughter_Birthday<<Daughter<<endl;
-    file <<Daughter_Birthday<<Son<<endl;
-    file <<Daughter_Birthday<<Birthday_Son<<endl;
-    /*_*/
-    file <<Daughter_Birthday<<"_"<<Name<<endl;
-    file <<Daughter_Birthday<<"_"<<Surname<<endl;
-    file <<Daughter_Birthday<<"_"<<Date<<endl;
-    file <<Daughter_Birthday<<"_"<<Pet<<endl;
-    file <<Daughter_Birthday<<"_"<<Daughter<<endl;
-    file <<Daughter_Birthday<<"_"<<Son<<endl;
-    file <<Daughter_Birthday<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Daughter_Birthday<<"-"<<Name<<endl;
-    file <<Daughter_Birthday<<"-"<<Surname<<endl;
-    file <<Daughter_Birthday<<"-"<<Date<<endl;
-    file <<Daughter_Birthday<<"-"<<Pet<<endl;
-    file <<Daughter_Birthday<<"-"<<Daughter<<endl;
-    file <<Daughter_Birthday<<"-"<<Son<<endl;
-    file <<Daughter_Birthday<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Daughter_Birthday<<"."<<Name<<endl;
-    file <<Daughter_Birthday<<"."<<Surname<<endl;
-    file <<Daughter_Birthday<<"."<<Date<<endl;
-    file <<Daughter_Birthday<<"."<<Pet<<endl;
-    file <<Daughter_Birthday<<"."<<Daughter<<endl;
-    file <<Daughter_Birthday<<"."<<Son<<endl;
-    file <<Daughter_Birthday<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Daughter_Birthday<<" "<<Name<<endl;
-    file <<Daughter_Birthday<<" "<<Surname<<endl;
-    file <<Daughter_Birthday<<" "<<Date<<endl;
-    file <<Daughter_Birthday<<" "<<Pet<<endl;
-    file <<Daughter_Birthday<<" "<<Daughter<<endl;
-    file <<Daughter_Birthday<<" "<<Son<<endl;
-    file <<Daughter_Birthday<<" "<<Birthday_Son<<endl;
-
-    /*Son generating*/
-    file <<Son<<Name<<endl;
-    file <<Son<<Surname<<endl;
-    file <<Son<<Date<<endl;
-    file <<Son<<Pet<<endl;
-    file <<Son<<Daughter<<endl;
-    file <<Son<<Daughter_Birthday<<endl;
-    file <<Son<<Birthday_Son<<endl;
-    /*_*/
-    file <<Son<<"_"<<Name<<endl;
-    file <<Son<<"_"<<Surname<<endl;
-    file <<Son<<"_"<<Date<<endl;
-    file <<Son<<"_"<<Pet<<endl;
-    file <<Son<<"_"<<Daughter<<endl;
-    file <<Son<<"_"<<Daughter_Birthday<<endl;
-    file <<Son<<"_"<<Birthday_Son<<endl;
-    /*-*/
-    file <<Son<<"-"<<Name<<endl;
-    file <<Son<<"-"<<Surname<<endl;
-    file <<Son<<"-"<<Date<<endl;
-    file <<Son<<"-"<<Pet<<endl;
-    file <<Son<<"-"<<Daughter<<endl;
-    file <<Son<<"-"<<Daughter_Birthday<<endl;
-    file <<Son<<"-"<<Birthday_Son<<endl;
-    /*.*/
-    file <<Son<<"."<<Name<<endl;
-    file <<Son<<"."<<Surname<<endl;
-    file <<Son<<"."<<Date<<endl;
-    file <<Son<<"."<<Pet<<endl;
-    file <<Son<<"."<<Daughter<<endl;
-    file <<Son<<"."<<Daughter_Birthday<<endl;
-    file <<Son<<"."<<Birthday_Son<<endl;
-    /* */
-    file <<Son<<" "<<Name<<endl;
-    file <<Son<<" "<<Surname<<endl;
-    file <<Son<<" "<<Date<<endl;
-    file <<Son<<" "<<Pet<<endl;
-    file <<Son<<" "<<Daughter<<endl;
-    file <<Son<<" "<<Daughter_Birthday<<endl;
-    file <<Son<<" "<<Birthday_Son<<endl;
-
-    /*Brithday_Son generating*/
-    file <<Birthday_Son<<Name<<endl;
-    file <<Birthday_Son<<Surname<<endl;
-    file <<Birthday_Son<<Date<<endl;
-    file <<Birthday_Son<<Pet<<endl;
-    file <<Birthday_Son<<Daughter<<endl;
-    file <<Birthday_Son<<Daughter_Birthday<<endl;
-    file <<Birthday_Son<<Son<<endl;
-    /*_*/
-    file <<Birthday_Son<<"_"<<Name<<endl;
-    file <<Birthday_Son<<"_"<<Surname<<endl;
-    file <<Birthday_Son<<"_"<<Date<<endl;
-    file <<Birthday_Son<<"_"<<Pet<<endl;
-    file <<Birthday_Son<<"_"<<Daughter<<endl;
-    file <<Birthday_Son<<"_"<<Daughter_Birthday<<endl;
-    file <<Birthday_Son<<"_"<<Son<<endl;
-    /*-*/
-    file <<Birthday_Son<<"-"<<Name<<endl;
-    file <<Birthday_Son<<"-"<<Surname<<endl;
-    file <<Birthday_Son<<"-"<<Date<<endl;
-    file <<Birthday_Son<<"-"<<Pet<<endl;
-    file <<Birthday_Son<<"-"<<Daughter<<endl;
-    file <<Birthday_Son<<"-"<<Daughter_Birthday<<endl;
-    file <<Birthday_Son<<"-"<<Son<<endl;
-    /*.*/
-    file <<Birthday_Son<<"."<<Name<<endl;
-    file <<Birthday_Son<<"."<<Surname<<endl;
-    file <<Birthday_Son<<"."<<Date<<endl;
-    file <<Birthday_Son<<"."<<Pet<<endl;
-    file <<Birthday_Son<<"."<<Daughter<<endl;
-    file <<Birthday_Son<<"."<<Daughter_Birthday<<endl;
-    file <<Birthday_Son<<"."<<Son<<endl;
-    /* */
-    file <<Birthday_Son<<" "<<Name<<endl;
-    file <<Birthday_Son<<" "<<Surname<<endl;
-    file <<Birthday_Son<<" "<<Date<<endl;
-    file <<Birthday_Son<<" "<<Pet<<endl;
-    file <<Birthday_Son<<" "<<Daughter<<endl;
-    file <<Birthday_Son<<" "<<Daughter_Birthday<<endl;
-    file <<Birthday_Son<<" "<<Son<<endl;
+world_list::combination_one(Name,Surname);
+world_list::combination_one(Name,Date);
+world_list::combination_one(Name,Pet);
+world_list::combination_one(Name,Daughter);
+world_list::combination_one(Name,Daughter_Birthday);
+world_list::combination_one(Name,Son);
+world_list::combination_one(Name,Birthday_Son);
+world_list::combination_one(Surname,Date);
+world_list::combination_one(Surname,Pet);
+world_list::combination_one(Surname,Daughter);
+world_list::combination_one(Surname,Daughter_Birthday);
+world_list::combination_one(Surname,Son);
+world_list::combination_one(Surname,Birthday_Son);
+world_list::combination_one(Date,Pet);
+world_list::combination_one(Date,Daughter);
+world_list::combination_one(Date,Daughter_Birthday);
+world_list::combination_one(Date,Son);
+world_list::combination_one(Date,Birthday_Son);
+world_list::combination_one(Pet,Daughter);
+world_list::combination_one(Pet,Daughter_Birthday);
+world_list::combination_one(Pet,Son);
+world_list::combination_one(Pet,Birthday_Son);
+world_list::combination_one(Daughter,Daughter_Birthday);
+world_list::combination_one(Daughter,Son);
+world_list::combination_one(Daughter,Birthday_Son);
+world_list::combination_one(Daughter_Birthday,Son);
+world_list::combination_one(Daughter_Birthday,Birthday_Son);
+world_list::combination_one(Son,Birthday_Son);
 };
 
 void world_list::start_choose(char choose) {
     cout << "Hello I'm World List Generator! :D" << endl;
-    cout << "1.Form OR 2.Random_Strings" << endl;
+    cout<<"Write everything with lower case!"<<endl;
+    cout << "1.Form       OR          2.Random_Strings(not complete!)" << endl;
     cin>>choose;
     if(choose == '1') {
         world_list::choose_form(Name,Surname,Date,Pet,Daughter,Daughter_Birthday,Son,Birthday_Son);
